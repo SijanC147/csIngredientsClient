e975edf4b905f9122cfebeb33c1e4046ff585917
Sean Bugeja <13831460+SijanC147@users.noreply.github.com>
2021-06-30 13:34:15 +0200

    adds env variable for production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: true,
   appVersion: 'v717demo1',
   USERDATA_KEY: 'authf649fc9a5f55',
-  isMockEnabled: true,
-  apiUrl: 'api'
+  isMockEnabled: false,
+  apiUrl: process.env.API_ENDPOINT
   // apiUrl: 'mysite.com/api'
 };
