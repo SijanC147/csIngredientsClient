4e927b0cef6b9cf1c28d3dbb4a1b31f10429db5e
Sean Bugeja <13831460+SijanC147@users.noreply.github.com>
2021-07-02 12:21:56 +0200

    disable authguard in prod

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
   },
   {
     path: '',
-    canActivate: [AuthGuard],
+    // canActivate: [AuthGuard],
     loadChildren: () =>
       import('./pages/layout.module').then((m) => m.LayoutModule),
   },
