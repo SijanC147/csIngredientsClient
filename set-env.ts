a7b65d4344b8ce09c0572586171923d6a096024a
Sean Bugeja <13831460+SijanC147@users.noreply.github.com>
2021-07-02 12:11:53 +0200

    enable mock in prod to bypass auth

--- set-env.ts
+++ set-env.ts
@@ -21,7 +21,7 @@ export const environment = {
         `production: true,
     appVersion: 'v717demo1',
     USERDATA_KEY: 'authf649fc9a5f55',
-    isMockEnabled: false`)}
+    isMockEnabled: true`)}
 };`;
 fs.writeFile(targetPath, envConfigFile, function (err) {
     if (err) {
