db0cd1c3c7f3e0ffc43312c70eb2981a96f9c5d7
Sean Bugeja <13831460+SijanC147@users.noreply.github.com>
2021-07-02 10:51:29 +0200

    fix typo in setenv for prod builds

--- set-env.ts
+++ set-env.ts
@@ -1,11 +1,12 @@
 const fs = require('fs')
 const devEnv = process.argv[process.argv.length - 1] === "dev"
-const targetFileName = `environment.${devEnv ? "" : "prod"}ts`
+const targetFileName = `environment.${devEnv ? "" : "prod."}ts`
 const targetPath = `./src/environments/${targetFileName}`;
+// temporarily disabled auth on deployment until it's fixed
 const envConfigFile = `
 export const environment = {
     apiUrl: '${process.env.API_ENDPOINT}',
-    authDomain: '${process.env.AUTH_DOMAIN}',
+    authDomain: '${process.env.AUTH_DOMAIN || "api"}', 
     identityPoolId: '${process.env.IDENTITY_POOL_ID}',
     region: '${process.env.REGION}',
     identityPoolRegion: '${process.env.REGION}',
